b4bfdfddafdc1cd3fd5f9985d8e855007114256f
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-09-29 10:40:10 -0300

    atualização do exercício

---
+++ exercicio2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void maxmin(int vetor[], int n, int &maximo, int &minimo) {
+}
+
+void imprimir(int vetor[], int tamVetor) {
+}
+
+int main() {
+    /*
+        1- Foi percebido que na função/procedimento maxmin há uma alta coesão quando falamos de encontrar o valor máximo e mínimo, pois, se trata 02 elementos em busca
+        da mesma tarefa;
+        2- Já em relação ao acoplamento foi entendido que as 02 funções(procediemntos) possui um grau de dependência baixo, logo possuem baixo acoplamento. 
+    */
+
+    return 0;
+}
