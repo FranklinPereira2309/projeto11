8a550dcac5ce2be914c87595e58ec65ca59c968c
FranklinMenezes2309 <fmpereira2309@hotmail.com>
2023-09-29 10:27:35 -0300

    refatoramento

--- exercicio1.cpp
+++ exercicio1.cpp
@@ -5,7 +5,10 @@ using namespace std;
 
 void maxmin(int vetor[], int n, int &maximo, int &minimo) {
     
-    maximo = minimo = vetor[0];
+    //maximo = vetor[0];
+    //minimo = vetor[0];
+
+    minimo = maximo = vetor[0];
 
     for(int i=1 ; i<n ; i++) {
         if(vetor[i] > maximo) {
@@ -16,33 +19,33 @@ void maxmin(int vetor[], int n, int &maximo, int &minimo) {
             }
         }
     }
-        cout << "Valor Mínimo: " << minimo << endl;
-        cout << "Valor Máximo: " << maximo << endl;
+        cout << "Esse é o valor Mínimo: " << minimo << endl;
+        cout << "Esse é o Valor Máximo: " << maximo << endl;
 }
 
-void print_vetor(int vetorTeste[], int tamanhdoVetor) {
-    cout << "Vetor" << endl;
-    for(int i=0; i < tamanhdoVetor; i++) {
-        cout << vetorTeste[i] << ", ";
+void imprimir(int vetor[], int tamVetor) {
+    cout << "\nValores do Vetor: " << endl;
+    for(int i=0; i < tamVetor; i++) {
+        cout << vetor[i] << ", ";
     }
     cout << endl;
 }
 
 int main() {
-    int tamanhoVetor = 10;
+    int tamVetor = 10;
     int maximo;
     int minimo;
-    int vetorTeste[tamanhoVetor] = {0};
+    int vetorPadrao[tamVetor];
 
     srand(time(nullptr));
 
-    for(int i=0; i < tamanhoVetor; i++) {
-        vetorTeste[i] = rand()%43;
+    for(int i=0; i < tamVetor; i++) {
+        vetorPadrao[i] = rand()%43;
     }
 
-    cout << "--------------------------------------" << endl;
-    maxmin(vetorTeste, tamanhoVetor, maximo, minimo);
-    print_vetor(vetorTeste, tamanhoVetor);
+    cout << "===========================================\n";
+    maxmin(vetorPadrao, tamVetor, maximo, minimo);
+    imprimir(vetorPadrao, tamVetor);
 
     return 0;
 }
